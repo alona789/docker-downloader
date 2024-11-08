887c12f2a47393dc15bc709c761736d07f17364a
xiatian <openhello@foxmail.com>
2024-11-08 09:15:15 +0800

    fix: 修复一些问题，增加过程日志

--- src/main/java/org/example/dockerdownloader/engine/DockerAuthApi.java
+++ src/main/java/org/example/dockerdownloader/engine/DockerAuthApi.java
@@ -1,9 +1,11 @@
 package org.example.dockerdownloader.engine;
 
 import com.dtflys.forest.annotation.Get;
+import com.dtflys.forest.annotation.HTTPProxy;
 import com.dtflys.forest.annotation.Var;
 import org.example.dockerdownloader.engine.response.AuthResp;
 
+@HTTPProxy(host = "127.0.0.1", port = "10809")
 public interface DockerAuthApi {
 
     @Get(url = "{url}?service={service}&scope=repository:{repository}:pull")
--- src/main/java/org/example/dockerdownloader/engine/DockerClient.java
+++ src/main/java/org/example/dockerdownloader/engine/DockerClient.java
@@ -13,6 +13,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 import static com.dtflys.forest.Forest.client;
@@ -42,16 +43,18 @@ public class DockerClient {
         log.info("tempDirectory: {}", tempDirectory);
 
         try {
-
+            log.info("get manifest");
             ManifestResp tagManifests = registryApi.manifest(registry, imageName, tag, VND_DOCKER_DISTRIBUTION_MANIFEST_V2_JSON);
             log.debug("resp: {}", tagManifests);
 
             ManifestResp.Manifests manifests = tagManifests.getManifests().toArray(new ManifestResp.Manifests[0])[0];
             // getFsLayers
+            log.info("get FsLayers");
             ManifestResp manifest = registryApi.manifest(registry, imageName, manifests.getDigest(), VND_OCI_IMAGE_MANIFEST_V1_JSON);
 
             // getDockerConfig
             // https://github.com/opencontainers/image-spec/blob/v1.0.1/config.md
+            log.info("download docker config");
             ManifestResp.Config config = manifest.getConfig();
             InputStream configInputStream = registryApi.blobs(registry, imageName, config.getDigest());
 //        InputStream configInputStream = registryApi.downloadManifest(DOCKER_REGISTRY_URL, imageName, config.getDigest(), config.getMediaType());
@@ -59,11 +62,13 @@ public class DockerClient {
             Digests digests = new Digests(config.getDigest());
             String configJsonFileName = String.format("%s.json", digests.getDigest());
 //        Files.write(tempDirectory.resolve(configJsonFileName), configBytes);
-            Files.copy(configInputStream, tempDirectory.resolve(configJsonFileName));
+            log.info("write config json to {}", tempDirectory.resolve(configJsonFileName));
+            Files.copy(configInputStream, tempDirectory.resolve(configJsonFileName), StandardCopyOption.REPLACE_EXISTING);
             IOUtils.close(configInputStream);
 
             ManifestFileDTO manifestFileDTO = new ManifestFileDTO();
 
+            log.info("get layers");
             for (ManifestResp.Layers layer : manifest.getLayers()) {
 
                 // 创建layer文件夹
@@ -75,17 +80,23 @@ public class DockerClient {
                 // 创建VERSION 文件
                 Files.write(layerDirPath.resolve("VERSION"), "1.0".getBytes());
 
-                // 下载layer.tar
-                InputStream layerInputSteam = registryApi.blobs(registry, imageName, digest);
+                Path layerTarPath = layerDirPath.resolve("layer.tar");
+
+                // layer不存在，下载
+                // todo 未校验完整性
+                if (!layerTarPath.toFile().exists()) {
+                    // 下载layer.tar
+                    InputStream layerInputSteam = registryApi.blobs(registry, imageName, digest);
 //            Path layerTarGzPath = layerDirPath.resolve("layer.tar.gz");
 //            Files.copy(layerInputSteam, layerTarGzPath);
 //            IOUtils.close(layerInputSteam);
 
-                // gz 解压
-                Path layerTarPath = layerDirPath.resolve("layer.tar");
+                    // gz 解压
 //            decompressGzipFile(layerTarGzPath.toString(), layerTarPath.toString());
-                decompressGzipFile(layerInputSteam, layerTarPath.toString());
-                IOUtils.close(layerInputSteam);
+                    decompressGzipFile(layerInputSteam, layerTarPath.toString());
+                    IOUtils.close(layerInputSteam);
+                }
+
 
                 String layerItem = layerTarPath.toString().replace(tempDirectory.toString() + File.separatorChar, "")
                         .replace(File.separatorChar, '/');
@@ -98,6 +109,7 @@ public class DockerClient {
             manifestFileDTO.getRepoTags().add(String.format("%s:%s", imageName, tag));
             manifestFileDTO.setConfig(configJsonFileName);
 
+            log.info("write manifest.json");
             Files.write(tempDirectory.resolve("manifest.json"), JsonUtil.toJson(List.of(manifestFileDTO)).getBytes());
 
 //        Path imageFile = Files.createTempFile("docker_pull_cache_", ArchiveStreamFactory.TAR);
@@ -108,11 +120,17 @@ public class DockerClient {
 //        IOUtils.close(input);
 
             Path targetTarPath = Paths.get(tempDirectory.getFileName() + ".tar");
+
+            log.info("packaging");
             createTarGzipFolder(tempDirectory, targetTarPath);
+
             return targetTarPath;
         } catch (Exception e) {
-            FileUtils.delete(tempDirectory.toFile());
+            log.error("pull image error", e);
             throw e;
+        } finally {
+            log.info("delete temp directory");
+//            FileUtils.deleteDirectory(tempDirectory.toFile());
         }
     }
 }
